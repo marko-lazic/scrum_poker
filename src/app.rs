93f1f2c05694f3d78dc565352fd3e2445b66abcf
Lazić <markolazic@protonmail.com>
2023-12-25 18:34:16 +0100

    Add padding x to the div containing cards

--- src/app.rs
+++ src/app.rs
@@ -11,8 +11,8 @@ pub fn App(cx: Scope) -> Element {
                 class: "absolute left-1/2 top-1/2 max-w-none -translate-x-1/2 -translate-y-1/2",
                 width: "1308"
             }
-            div { class: "absolute inset-0 bg-[url(/public/grid.svg)] bg-center [mask-image:linear-gradient(180deg,white,rgba(255,255,255,0))]" }
-            div { class: "sm:mx-auto sm:max-w-4xl sm:rounded-lg sm:px-10",
+            div { class: "absolute  inset-0 bg-[url(/public/grid.svg)] bg-center [mask-image:linear-gradient(180deg,white,rgba(255,255,255,0))]" }
+            div { class: "sm:mx-auto sm:max-w-4xl sm:rounded-lg px-10 sm:py-10",
                 div { class: "divide-y divide-gray-300/50",
                     div { class: "flex flex-wrap gap-4",
                         Card { value: "?".into() }
