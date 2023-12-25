ce7ef536309bcceaa2fa0531b4fc570bfab8b395
Lazić <markolazic@protonmail.com>
2023-12-25 17:23:02 +0100

    Fix cards appearence on small screen

--- src/card.rs
+++ src/card.rs
@@ -10,12 +10,12 @@ pub struct CardProps {
 pub fn Card(cx: Scope<CardProps>) -> Element {
     cx.render(rsx! {
         button {
-            class: "select-none p-1 relative sm:w-10 md:w-20 sm:h-14 md:h-28 mx-auto bg-white hover:bg-slate-100 focus:bg-slate-400 rounded-xl shadow-lg text-2xl md:text-3xl text-slate-500 focus:text-slate-50",
+            class: "select-none p-1 relative w-12 md:w-20 h-14 md:h-28 mx-auto bg-white hover:bg-slate-100 focus:bg-slate-400 rounded-xl shadow-lg text-2xl md:text-3xl text-slate-500 focus:text-slate-50",
             onclick: move |_| {
                 println!("Clicked {:?}", cx.props.value);
             },
             span {
-                div { class: "flex flex-col w-full h-full md:items-stretch md:justify-between sm:items-center sm:justify-center",
+                div { class: "flex flex-col w-full h-full justify-between",
                     div { class: "hidden md:flex justify-between",
                         img {
                             class: "block mx-auto h-3 sm:mx-0 sm:shrink-0",
@@ -26,7 +26,7 @@ pub fn Card(cx: Scope<CardProps>) -> Element {
                             src: "/public/logo_trans.png"
                         }
                     }
-                    "{cx.props.value}"
+                    div { class: "m-auto", "{cx.props.value}" }
                     div { class: "hidden md:flex justify-between",
                         img {
                             class: "block mx-auto h-3 sm:mx-0 sm:shrink-0",
