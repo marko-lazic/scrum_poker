15fc1358c66cb73902dc9dadc7a3b66f1f6b9d56
Lazić <markolazic@protonmail.com>
2023-12-25 12:11:19 +0100

    Shrink cards on smaller screens

--- src/card.rs
+++ src/card.rs
@@ -7,10 +7,10 @@ pub struct CardProps {
 
 pub fn Card(cx: Scope<CardProps>) -> Element {
     cx.render(rsx! {
-        button { class: "p-1 relative w-20 h-28 mx-auto bg-white hover:bg-slate-100 focus:bg-stone-300 rounded-xl shadow-lg",
+        button { class: "select-none p-1 relative sm:w-10 md:w-20 sm:h-14 md:h-28 mx-auto bg-white hover:bg-slate-100 focus:bg-slate-400 rounded-xl shadow-lg text-2xl md:text-3xl text-slate-500 focus:text-slate-50",
             span {
-                div { class: "w-full h-full flex flex-col justify-between",
-                    div { class: "flex justify-between",
+                div { class: "flex flex-col w-full h-full md:items-stretch md:justify-between sm:items-center sm:justify-center",
+                    div { class: "hidden md:flex justify-between",
                         img {
                             class: "block mx-auto h-3 sm:mx-0 sm:shrink-0",
                             src: "/public/logo_trans.png"
@@ -20,8 +20,8 @@ pub fn Card(cx: Scope<CardProps>) -> Element {
                             src: "/public/logo_trans.png"
                         }
                     }
-                    div { class: "flex justify-center text-3xl text-slate-500", "{cx.props.value}" }
-                    div { class: "flex justify-between",
+                    "{cx.props.value}"
+                    div { class: "hidden md:flex justify-between",
                         img {
                             class: "block mx-auto h-3 sm:mx-0 sm:shrink-0",
                             src: "/public/logo_trans.png"
